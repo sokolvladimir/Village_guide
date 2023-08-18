6fa34e46526a86fd51be5f148e903441d007fd42
Uladzimir Sokal <sokolvladimir-by@mail.ru>
2023-08-18 09:27:00 +0300

    fix

--- app/config.py
+++ app/config.py
@@ -8,9 +8,9 @@ API_TOKEN = os.environ.get('token', None)
 
 DATABASE = {
     'drivername': 'postgresql+psycopg2',
-    'host': os.environ.get('DB_HOST', 'localhost'),
-    'port': os.environ.get('DB_PORT', '5432'),
-    'username': os.environ.get('DB_USER', 'postgres'),
-    'password': os.environ.get('DB_PASS', 'postgres'),
-    'database': os.environ.get('POSTGRES_DB', 'village_guide')
+    'host': os.environ.get('POSTGRES_HOST', 'localhost'),
+    'port': os.environ.get('POSTGRES_PORT', '5432'),
+    'username': os.environ.get('POSTGRES_USER', 'postgres'),
+    'password': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+    'database': os.environ.get('POSTGRES_NAME', 'village_guide')
 }
